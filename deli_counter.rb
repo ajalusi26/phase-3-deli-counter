55bd2ede282727cc2215c45dbbcd4c92ea6120e3
AJ Jalusi <ajalusi26@gmail.com>
2022-01-31 11:42:16 -0500

    commit tree 839f1922f5a55088ccfec20ab0c20a765b64fec7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+def line katz_deli
+    if katz_deli.length > 0
+        p "The line is currently: "
+        katz_deli.each { |item| p "#{katz_deli.index(item) + 1}. #{item} " }
+    else
+        p "The line is empty."
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.append(name)
+    p "#{name}, you are number #{katz_deli.size} in line"
+end
+
+
+def now_serving(katz_deli)
+    p "Now serving #{katz_deli[0]}"
+    katz_deli.shift()
+end
+
